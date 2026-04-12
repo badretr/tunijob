f8c4233a6aaf18f2b6425e14f74750aac8c55cb9
pfa <pfa@pfa.myguest.virtualbox.org>
2026-04-12 15:47:54 +0100

    Ajouter route sentry-debug pour test

--- core/urls.py
+++ core/urls.py
@@ -4,12 +4,17 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+def trigger_error(request):
+    1 / 0
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls')),
     path('dashboard/', include('apps.dashboard.urls')),
     # Redirect legacy /catalog/ requests to jobs list
     path('catalog/', RedirectView.as_view(pattern_name='jobs:job_list', permanent=False), name='catalog_redirect'),
+	path('sentry-debug/', trigger_error, name='sentry_debug'),
 
     path('', include('apps.jobs.urls')),  # Keep only this one for jobs URLs
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
