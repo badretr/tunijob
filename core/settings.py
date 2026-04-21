ab5f0f4ca1e82e005969c18d6a1ef3cff00f6355
pfa <pfa@pfa.myguest.virtualbox.org>
2026-04-21 18:19:03 +0100

    image home2

--- core/settings.py
+++ core/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
 }
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/opt/tunijob/staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
